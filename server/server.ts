cb2adcc24509624d678961382fc791918535d57a
elisa-bertoni <89889937+elisabertoni@users.noreply.github.com>
2023-03-23 17:02:17 +1300

    create style.css and link it to html

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,8 @@
 import { join } from 'node:path'
 import express from 'express'
-
+import 'dotenv/config'
 import welcome from './routes/welcome'
+import request from 'superagent'
 
 const server = express()
 
@@ -9,5 +10,12 @@ server.use(express.json())
 server.use(express.static(join(__dirname, './public')))
 
 server.use('/api/v1/welcome', welcome)
+
+
+
+server.get('/api/chat', async (req ,res) => {
+  const response = await request.get('https://api.openai.com/v1/chat/completions')
+  return response
+})
 
 export default server
