6c9affce37bde6406ca2b12049d25a679c3bb4cb
Keri van der Westhuizen <75972814+kerivdw@users.noreply.github.com>
2023-03-23 17:11:18 +1300

    clean up the server request as it needs to move to the client api

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,7 @@ import { join } from 'node:path'
 import express from 'express'
 import cors from 'cors'
 
-import 'dotenv/config'
 import welcome from './routes/welcome'
-import request from 'superagent'
 import chatgpt from './routes/chatgpt'
 
 const server = express()
@@ -14,13 +12,6 @@ server.use(express.json())
 server.use(express.static(join(__dirname, './public')))
 
 server.use('/api/v1/welcome', welcome)
-
-
-
-server.get('/api/chat', async (req ,res) => {
-  const response = await request.get('https://api.openai.com/v1/chat/completions')
-  return response
-})
 server.use('/api/v1/gpt', chatgpt)
 
 export default server
